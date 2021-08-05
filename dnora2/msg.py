07e416d05f850e0f30f0d3fb2e7a15c3b2e0106d
Jan-Victor Björkqvist <jan-victor.bjorkqvist@met.no>
2021-08-05 16:01:59 +0200

    Forgot to add files in previous commit

---
+++ dnora2/msg.py
@@ -0,0 +1,27 @@
+def print_line(length = 75):
+    print("-" * length)
+
+def to_file(filename):
+    info(f"Writing to file {filename}")
+    
+def plain(msg):
+    print(msg)
+
+def info(msg):
+    print(f"*** {msg} ***")
+
+def advice(msg):
+    print(f"!!! {msg} !!!")
+    
+    
+def templates(code):
+    if code == 'no_spacing':
+        info("No information about grid spacing exists")
+        advice("First set spacing with .set_spacing(dlon, dlat) or .set_spacing(dm) (in metres)")
+    elif code == 'no_topo':
+        info("No topography exists")
+        advice("First load topography with .import_topo(topography_fetcher)")
+    elif code == 'no_mask':
+        info("No land-sea mask exists")
+        advice("First load topography with .import_topo(topography_fetcher)")
+
