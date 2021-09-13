ca53ef3f63857af2820ab31531c85a7591f33fa1
Jan-Victor Björkqvist <jan-victor.bjorkqvist@met.no>
2021-09-13 15:40:24 +0200

    Added MyWave3km wind forcing fetcher

--- dnora2/wnd.py
+++ dnora2/wnd.py
@@ -8,6 +8,13 @@ from subprocess import call
 import xarray as xr
 import os
 
+
+def u_v_from_dir(ws,wdir):
+# see http://tornado.sfsu.edu/geosciences/classes/m430/Wind/WindDirection.html
+    u = -ws * (np.sin(np.deg2rad(wdir)))
+    v = -ws * (np.cos(np.deg2rad(wdir)))
+    return u,v
+
 # =============================================================================
 # FORCING FETCHER CLASSES RESPONSIBLE FOR ACTUALLY READING THE SPECTRA
 # =============================================================================
@@ -43,6 +50,125 @@ class ForcingFetcher(ABC):
 
     def __str__(self):
         return (f"{self.start_time} - {self.end_time}")
+
+
+
+
+
+
+
+class ForcingMyWave3km(ForcingFetcher):
+    def __init__(self, stride = 24, hours_per_file = 24, last_file = None, lead_time = 0):
+        self.stride = copy(stride)
+        self.hours_per_file = copy(hours_per_file)
+        self.lead_time = copy(lead_time)
+        self.last_file = copy(last_file)
+        return
+
+
+    def __call__(self, grid, start_time, end_time, expansion_factor):
+        """Reads in all boundary spectra between the given times and at for the given indeces"""
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+        #days = bnd.day_list(start_time = self.start_time, end_time = self.end_time)
+        start_times, end_times, file_times = create_time_stamps(start_time, end_time, self.stride, self.hours_per_file, self.last_file, self.lead_time)
+
+        wnd_list =[]
+
+        temp_folder = 'dnora_wnd_temp'
+        if not os.path.isdir(temp_folder):
+            os.mkdir(temp_folder)
+            print ("Creating folder %s..." % temp_folder)
+
+        msg.info(f"Getting wind forcing from MEPS from {self.start_time} to {self.end_time}")
+        for n in range(len(file_times)):
+            #print(time_stamp)
+            #print(days[n].strftime('%Y-%m-%d'))
+            url = self.get_url(file_times[n])
+
+            msg.info(url)
+            msg.plain(f"Reading wind forcing data: {start_times[n]}-{end_times[n]}")
+
+
+
+
+            nc_fimex = f'dnora_wnd_temp/wind_{n:04.0f}.nc'
+            #nc_fimex = 'dnora_wnd_temp.nc'
+
+
+            # Define area to search in
+            expand_lon = (grid.lon()[-1] - grid.lon()[0])*(expansion_factor-1)*0.5
+            expand_lat = (grid.lat()[-1] - grid.lat()[0])*(expansion_factor-1)*0.5
+
+            lon_min = grid.lon()[0] - expand_lon
+            lon_max = grid.lon()[-1] + expand_lon
+
+            lat_min = grid.lat()[0] - expand_lat
+            lat_max = grid.lat()[-1] + expand_lat
+
+            dlon = grid.data.dlon*5
+            dlat = grid.data.dlat*5
+
+
+            fimex_command = ['fimex-1.6', '--input.file='+url,
+                  '--interpolate.method=bilinear',
+                  '--interpolate.projString=+proj=latlong +ellps=sphere +a=6371000 +e=0',
+                  '--interpolate.xAxisValues='+str(lon_min)+','+str(lon_min+dlon)+',...,'+str(lon_max)+'',
+                  '--interpolate.yAxisValues='+str(lat_min)+','+str(lat_min+dlat)+',...,'+str(lat_max)+'',
+                  '--interpolate.xAxisUnit=degree', '--interpolate.yAxisUnit=degree',
+                  #'--extract.reduceToBoundingBox.south=' + str(lat_min),
+                  #'--extract.reduceToBoundingBox.north=' + str(lat_max),
+                  #'--extract.reduceToBoundingBox.west=' + str(lon_min),
+                  #'--extract.reduceToBoundingBox.east=' + str(lon_max),
+                  '--process.rotateVector.all',
+                  '--extract.selectVariables=ff','--extract.selectVariables=dd',
+                  #'--extract.selectVariables=latitude','--extract.selectVariables=longitude',
+                  '--extract.reduceTime.start='+start_times[n].strftime('%Y-%m-%dT%H:%M:%S'),
+                  '--extract.reduceTime.end='+end_times[n].strftime('%Y-%m-%dT%H:%M:%S'),
+                  '--process.rotateVector.direction=latlon',
+                  '--output.file='+nc_fimex]
+
+            #if self.prefix == 'subset':
+            #    fimex_command.insert(-2,'--extract.reduceDimension.name=ensemble_member')
+            #    fimex_command.insert(-2,'--extract.reduceDimension.start=1')
+            #    fimex_command.insert(-2,'--extract.reduceDimension.end=1')
+
+            call(fimex_command)
+            wnd_list.append(xr.open_dataset(nc_fimex).squeeze())
+
+        wind_forcing = xr.concat(wnd_list, dim="time")
+
+        # Fimex has already rotated the longitudes and latitudes, so calling them rlon/rlat is now incorrect        
+        wind_forcing = wind_forcing.rename_dims({'rlat': 'lat', 'rlon': 'lon'})
+        wind_forcing = wind_forcing.rename_vars({'rlat': 'lat', 'rlon': 'lon'})        
+
+        # Go to u and v components
+        u,v = u_v_from_dir(wind_forcing.ff, wind_forcing.dd) # factor 1000
+        u = u.fillna(0)
+        v = v.fillna(0)
+   
+        # Remove speed and dir and add components to dataset        
+        wind_forcing = wind_forcing.drop_vars(['ff', 'dd'])
+        wind_forcing["u"]=(['time', 'lat', 'lon'],  u)
+        wind_forcing["v"]=(['time', 'lat', 'lon'],  v) 
+        #wind_forcing.to_netcdf('test.nc')
+        return wind_forcing
+
+
+
+    def get_url(self, time_stamp):
+        filename = time_stamp.strftime('%Y')+time_stamp.strftime('%m')+time_stamp.strftime('%d')+'_MyWam3km_hindcast.nc'
+        url = 'https://thredds.met.no/thredds/dodsC/windsurfer/mywavewam3km_files/'+time_stamp.strftime('%Y')+'/'+time_stamp.strftime('%m')+'/'+filename
+        return url
+
+
+
+
+
+
+
 
 
 
@@ -105,7 +231,7 @@ class ForcingMEPS(ForcingFetcher):
             dlon = grid.data.dlon*5
             dlat = grid.data.dlat*5
 
-            
+
             fimex_command = ['fimex-1.6', '--input.file='+url,
                   '--interpolate.method=bilinear',
                   '--interpolate.projString=+proj=latlong +ellps=sphere +a=6371000 +e=0',
@@ -123,7 +249,7 @@ class ForcingMEPS(ForcingFetcher):
                   '--extract.reduceTime.end='+end_times[n].strftime('%Y-%m-%dT%H:%M:%S'),
                   '--process.rotateVector.direction=latlon',
                   '--output.file='+nc_fimex]
-            
+
             if self.prefix == 'subset':
                 fimex_command.insert(-2,'--extract.reduceDimension.name=ensemble_member')
                 fimex_command.insert(-2,'--extract.reduceDimension.start=1')
@@ -133,7 +259,7 @@ class ForcingMEPS(ForcingFetcher):
             wnd_list.append(xr.open_dataset(nc_fimex).squeeze())
 
         wind_forcing = xr.concat(wnd_list, dim="time")
-        
+
 
         #wind_forcing.to_netcdf('test.nc')
         return wind_forcing
