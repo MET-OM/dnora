80b1a5736f1ea3125a9b34e408a54fda7917c8b8
KonstantinChri <konstantinosc@met.no>
2021-11-16 22:23:08 +0100

    -

--- dnora/run.py
+++ dnora/run.py
@@ -4,7 +4,8 @@ from subprocess import Popen
 # RUN THE WAVE MODEL
 # =============================================================================
 
-def run_SWAN(input_file,swan_directory):
+
+def run_SWAN(input_file, swan_directory):
     print('Running SWAN----------------------->>>>>>>>>>>>>>>>>>>>>>>>>>')
-    p = Popen(['./swanrun','-input', input_file], cwd=swan_directory)
+    p = Popen(['./swanrun', '-input', input_file], cwd=swan_directory)
     p.wait()
