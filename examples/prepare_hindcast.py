6c361196d4cccf8fcc0ac9ba2aaea41bda2fa40f
KonstantinChri <konstantinosc@met.no>
2021-11-16 22:22:32 +0100

    change example to Boknafjorden

--- examples/prepare_hindcast.py
+++ examples/prepare_hindcast.py
@@ -12,11 +12,11 @@ from dnora import grd, bnd, wnd, inp, run
 # =============================================================================
 
 # Set grid definitions
-lon_min = 4.5
-lat_min = 60.83
-lon_max = 5.5
-lat_max = 61.25
-grid = grd.Grid(lon_min, lon_max, lat_min, lat_max, name='Skjerjehamn250')
+lon_min = 4.93
+lat_min = 58.7
+lon_max = 6.22
+lat_max = 59.45
+grid = grd.Grid(lon_min, lon_max, lat_min, lat_max, name='Bokn')
 
 # Set spacing and boundary points
 grid.set_spacing(dm=1000)
@@ -26,7 +26,8 @@ bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'], step=1)
 grid.set_boundary(boundary_setter=bnd_set)
 
 # Import topography and mesh it down to the grid definitions
-grid.import_topo(topo_reader=grd.read.EMODNET2018(tile='D5'))
+grid.import_topo(topo_reader=grd.read.EMODNET2018(tile='D5',
+                                   folder='/home/konstantinosc/PhD/github/DNORA/bathy/'))
 grid.mesh_grid()
 
 
@@ -38,8 +39,8 @@ grid.mesh_grid()
 boundary = bnd.Boundary(grid, name='NORA3')
 
 # Fetch the boundary spectra
-time0 = '2019-08-15T00:00'
-time1 = '2019-08-16T00:00'
+time0 = '2020-01-13T18:00'
+time1 = '2020-01-13T19:00'
 boundary.import_boundary(start_time=time0, end_time=time1, boundary_reader=bnd.read.MetNo_NORA3(), point_picker=bnd.pick.NearestGridPoint())
 
 # =============================================================================
@@ -55,7 +56,8 @@ forcing.import_forcing(start_time=time0, end_time=time1, forcing_reader=wnd.read
 # =============================================================================
 # WRITE OUTPUT FOR SWAN RUN
 # =============================================================================
-output_folder = 'example_hindcast'
+output_folder  = '/home/konstantinosc/test/'
+swan_directory = '/home/konstantinosc/Programs/swan4120/'
 # Grid
 write_grid = grd.write.SWAN(folder=output_folder)
 write_grid(grid)
@@ -69,13 +71,11 @@ write_forcing = wnd.write.SWAN(folder=output_folder)
 write_forcing(forcing)
 
 # Write input file for SWAN model run
-swan_directory = output_folder
 write_input_file = inp.SWANInputFile(grid=grid, forcing=forcing)
-input_file_name = write_input_file(
-    start_time=time0, end_time=time1, swan_directory=swan_directory, wind=True)
+input_file_name = write_input_file(start_time=time0, end_time=time1, swan_directory= swan_directory, forcing_folder = output_folder, wind=True)
 
 
 # =============================================================================
 # SWAN RUN
 # =============================================================================
-#run.run_SWAN(input_file_name, swan_directory=swan_directory)
+run.run_SWAN(input_file_name, swan_directory= swan_directory)
