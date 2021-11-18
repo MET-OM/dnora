b58f5bc5f470d2536878067fb4f788c18dcb4a43
KonstantinChri <konstantinosc@met.no>
2021-11-18 11:22:26 +0100

    add run_SWASH

--- dnora/run.py
+++ dnora/run.py
@@ -9,3 +9,9 @@ def run_SWAN(input_file, swan_directory):
     print('Running SWAN----------------------->>>>>>>>>>>>>>>>>>>>>>>>>>')
     p = Popen(['./swanrun', '-input', input_file], cwd=swan_directory)
     p.wait()
+
+
+def run_SWASH(input_file, swash_directory):
+    print('Running SWASH----------------------->>>>>>>>>>>>>>>>>>>>>>>>>>')
+    p = Popen(['./swashrun', '-input', input_file], cwd=swash_directory)
+    p.wait()
