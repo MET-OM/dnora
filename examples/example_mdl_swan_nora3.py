2d358558d1cf9bfc008f73488c262ad4f7d8694f
KonstantinChri <67804784+KonstantinChri@users.noreply.github.com>
2021-12-01 22:42:33 +0100

    Update example_mdl_swan_nora3.py
    add comments

--- examples/example_mdl_swan_nora3.py
+++ examples/example_mdl_swan_nora3.py
@@ -5,31 +5,41 @@ import sys
 dnora_directory = '../'
 sys.path.insert(0, dnora_directory)
 from dnora import bnd, grd, mdl, wnd
-
+# =============================================================================
+# DEFINE GRID OBJECT
+# =============================================================================
+# Set grid definitions
 lon_min = 4.0
 lat_min = 60.53
 lon_max = 5.73
 lat_max = 61.25
-
 grid = grd.Grid(lon_min, lon_max, lat_min, lat_max, name='Skjerjehamn250')
-
+# Set spacing and boundary points
 grid.set_spacing(dm=1000)
-bnd_set = grd.boundary.EdgesAsBoundary(edges = ['N', 'W', 'S'], step = 20)
+# Import topography and mesh it down to the grid definitions
+grid.import_topo(topo_reader=grd.read.EMODNET2018(tile='D5',
+                                                  folder='/home/konstantinosc/PhD/github/DNORA/bathy/'))
+grid.mesh_grid()
+# Set the boundaries
+bnd_set = grd.boundary.EdgesAsBoundary(edges = ['N', 'W', 'S'], step = 10)
 grid.set_boundary(boundary_setter = bnd_set)
 
 
 model = mdl.SWAN_NORA3(grid, start_time = '2018-08-25T00:00', end_time = '2018-08-25T01:00')
 
+# =============================================================================
+# IMPORT BOUNDARIES AND FORCING
+# =============================================================================
+model.import_boundary(name='NORA3')
+model.import_forcing(name='NORA3')
+# =============================================================================
+# WRITE OUTPUT FOR SWAN RUN
+# =============================================================================
 model.export_grid()
-
-model.import_boundary()
-model.import_forcing()
-
 model.export_boundary()
 model.export_forcing()
-
-# Uses the folder defined in defaults.py
 model.write_input_file()
-# Assums the model is located in the same folder as the input file was written to
-# This can be overridden with normal folder='#Forcing_#Grid' etc.
+# =============================================================================
+# SWASH RUN
+# =============================================================================
 model.run_model()
