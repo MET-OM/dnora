872ddf3bfd03621fab2b97cf1cbb83ad9fe1153c
KonstantinChri <konstantinosc@met.no>
2021-12-17 16:07:41 +0100

    correct spectral convection for SWAN to nautical(met)

--- dnora/bnd/write.py
+++ dnora/bnd/write.py
@@ -293,7 +293,7 @@ class SWAN(BoundaryWriter):
 
     def convention(self) -> str:
         """Convention of spectra"""
-        return 'Ocean'
+        return 'Met'
 
     def _preferred_format(self):
         """Format of filenames"""
