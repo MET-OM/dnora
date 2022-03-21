af8108445e360259a24584c084c1b6f2f960bcb5
Jan-Victor Björkqvist <janvb@met.no>
2022-03-21 17:21:29 +0100

    Started testing

--- examples/example_mdl_swan_nora3.py
+++ examples/example_mdl_swan_nora3.py
@@ -15,40 +15,42 @@ grid = grd.Grid(lon=(4.00, 5.73), lat=(60.53, 61.25), name='Skjerjehamn300')
 grid.set_spacing(dm=300)
 
 # Import topography and mesh it down to the grid definitions
-grid.import_topo(topo_reader=grd.read.EMODNET2018(tile='D5',
-                                    folder='/home/konstantinosc/bathy/'))
+#grid.import_topo(topo_reader=grd.read.EMODNET2018(tile='D5',
+#                                    folder='/home/konstantinosc/bathy/'))
+topo_reader=grd.read.EMODNET_MFDATA('/lustre/storeB/project/fou/om/WW3/bathy/emodnet_115m_x_115m/*2018.dtm')
+grid.import_topo(topo_reader=topo_reader)
 # This can be used to get an empty topography for testing
 #grid.import_topo(topo_reader=grd.read.EmptyTopo(grid=grid))
-
-grid.mesh_grid()
-
-# Set the boundaries
-bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'], step=20)
-grid.set_boundary(boundary_setter=bnd_set)
-
-
-# =============================================================================
-# DEFINE MODEL OBJECT
-# =============================================================================
-model = mdl.SWAN_NORA3(grid, start_time='2018-08-25T00:00',
-                             end_time='2018-08-25T03:00')
-# =============================================================================
-# IMPORT BOUNDARIES AND FORCING
-# =============================================================================
-model.import_boundary()
-model.import_forcing()
-# =============================================================================
-# PLOT GRID, FORCING AND BOUNDARIES
-# =============================================================================
-model.plot_grid()
-# =============================================================================
-# WRITE OUTPUT FOR SWAN RUN
-# =============================================================================
-model.export_grid()
-model.export_boundary()
-model.export_forcing()
-model.write_input_file()
-# =============================================================================
-# SWAN RUN
-# =============================================================================
-model.run_model()
+#
+# grid.mesh_grid()
+#
+# # Set the boundaries
+# bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'], step=20)
+# grid.set_boundary(boundary_setter=bnd_set)
+#
+#
+# # =============================================================================
+# # DEFINE MODEL OBJECT
+# # =============================================================================
+# model = mdl.SWAN_NORA3(grid, start_time='2018-08-25T00:00',
+#                              end_time='2018-08-25T03:00')
+# # =============================================================================
+# # IMPORT BOUNDARIES AND FORCING
+# # =============================================================================
+# model.import_boundary()
+# model.import_forcing()
+# # =============================================================================
+# # PLOT GRID, FORCING AND BOUNDARIES
+# # =============================================================================
+# model.plot_grid()
+# # =============================================================================
+# # WRITE OUTPUT FOR SWAN RUN
+# # =============================================================================
+# model.export_grid()
+# model.export_boundary()
+# model.export_forcing()
+# model.write_input_file()
+# # =============================================================================
+# # SWAN RUN
+# # =============================================================================
+# model.run_model()
