bd81e5688c6e92235a8cc56c4704890f168b4c0a
KonstantinChri <kchrist87@gmail.com>
2022-04-02 21:58:16 +0200

    update

--- examples/example_mdl_swash.py
+++ examples/example_mdl_swash.py
@@ -9,13 +9,13 @@ from dnora import grd, mdl, inp
 # DEFINE GRID OBJECT
 # =============================================================================
 # Set grid definitions
-grid = grd.Grid(lon=(12.25, 12.39), lat=(44.75, 44.85), name='Gorino25')
+grid = grd.Grid(lon=(4.93, 5.34), lat=(62.10, 62.24), name='Stad25')
 
 # Set spacing and boundary points
 grid.set_spacing(dm=25)
 
 # Import topography and mesh it down to the grid definitions
-grid.import_topo(topo_reader=grd.read.EMODNET2020(tile='E5',
+grid.import_topo(topo_reader=grd.read.EMODNET2020(tile='*',
                                     folder='/home/konstac/bathy/'))
 grid.mesh_grid()
 
@@ -34,7 +34,7 @@ model.plot_grid(save_fig=True,show_fig=False)
 # =============================================================================
 model.export_grid()
 model.write_input_file(input_file_writer=inp.SWASH(
-    bound_side_command='BOU SIDE S CCW CON REG 1.0 20 180 '))
+    bound_side_command='BOU SIDE N CCW CON REG 1.0 20 0 '))
 # =============================================================================
 # SWASH RUN
 # =============================================================================
