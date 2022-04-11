d1b16e432ed502fbd7c721bcde0cc280568afd8a
Jan-Victor Björkqvist <janvb@met.no>
2022-04-11 12:15:01 +0200

    examples

--- examples/example_mdl_reef3d.py
+++ examples/example_mdl_reef3d.py
@@ -1,5 +1,6 @@
 # REEF3D
 from dnora import grd, bnd, mdl
+import dnora.wave_parameters as wp
 
 use_raw = True
 
@@ -17,6 +18,11 @@ grid.set_boundary(boundary_setter=grd.boundary.MidPointAsBoundary(edges=['W']))
 model = mdl.REEF3D(grid, start_time='2019-03-05T06:00', end_time='2019-03-05T06:00')
 model.import_boundary(bnd.read_metno.NORA3())
 model.boundary_to_spectra()
+
+# h = wp.Hs()(model.spectra().data)
+# t = wp.Tm_10()(model.spectra().data)
+# print(f'Hs={h.hs.values[0][0]}m, Tm_10={t.tm_10.values[0][0]} s')
+
 model.export_spectra()
 
 model.export_grid(grd.write.REEF3D(use_raw=use_raw))  # Use when not meshed
