56f07781ede5d96075d9e6a09d210ed39d91e015
KonstantinChri <kchrist87@gmail.com>
2022-04-11 19:11:24 +0200

    update example

--- examples/example_mdl_swan_nora3.py
+++ examples/example_mdl_swan_nora3.py
@@ -16,7 +16,6 @@ grid.set_spacing(dm=300)
 
 # Import topography and mesh it down to the grid definitions
 
-<<<<<<< HEAD
 topo_reader=grd.read.EMODNET2020(tile='*',folder='/home/konstac/bathy/')
 grid.import_topo(topo_reader=topo_reader)
 
@@ -30,63 +29,28 @@ bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'], step=20)
 grid.set_boundary(boundary_setter=bnd_set)
 #
 #
-=======
-#grid.import_topo(topo_reader=grd.read.EMODNET2020(tile='*'))
-#topo_reader=grd.read.EMODNET2020(tile='*')
-#grid.import_topo(topo_reader=topo_reader)
-# This can be used to get an empty topography for testing
-#grid.import_topo(topo_reader=grd.read.EmptyTopo(grid=grid))
-#
-#grid.mesh_grid()
-#
-# Set the boundaries
-bnd_set = grd.boundary.MidPointAsBoundary(edges=['N', 'W', 'S'])
-grid.set_boundary(boundary_setter=bnd_set)
-
-
->>>>>>> origin/dev
 # =============================================================================
 # DEFINE MODEL OBJECT
 # =============================================================================
 model = mdl.SWAN_NORA3(grid, start_time='2018-08-25T00:00',
-<<<<<<< HEAD
                               end_time='2018-08-25T03:00')
-=======
-                             end_time='2018-08-25T00:00', dry_run=False)
->>>>>>> origin/dev
 # =============================================================================
 # IMPORT BOUNDARIES AND FORCING
 # =============================================================================
 model.import_boundary()
 model.import_forcing()
-<<<<<<< HEAD
 # =============================================================================
 # PLOT GRID, FORCING AND BOUNDARIES
 # =============================================================================
 model.plot_grid(save_fig=True, show_fig=False)
-=======
-model.boundary_to_spectra()
-# =============================================================================
-# PLOT GRID, FORCING AND BOUNDARIES
-# =============================================================================
-#model.plot_topo()
->>>>>>> origin/dev
 # =============================================================================
 # WRITE OUTPUT FOR SWAN RUN
 # =============================================================================
 model.export_grid()
 model.export_boundary()
 model.export_forcing()
-<<<<<<< HEAD
-=======
-model.export_spectra()
->>>>>>> origin/dev
 model.write_input_file()
 # =============================================================================
 # SWAN RUN
 # =============================================================================
-<<<<<<< HEAD
 model.run_model()
-=======
-model.run_model(dry_run=True)
->>>>>>> origin/dev
