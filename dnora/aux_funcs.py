6c05fd7d0e5c1432f33fe75dea498d22ded96fa4
Jan-Victor Björkqvist <janvb@met.no>
2022-06-24 14:03:55 +0200

    Added function to calculate coordinate for the SWAN SEGMENT command based on the boundary mask of a grid.

--- dnora/aux_funcs.py
+++ dnora/aux_funcs.py
@@ -504,3 +504,121 @@ def determine_patch_periods(times, start_time, end_time):
         inds[0:len(block)] = []
 
     return patch_start, patch_end
+
+def identify_boundary_edges(boundary_mask: np.ndarray) -> list[str]:
+    """Identifies which edges has some boundary points
+
+        North = [-1,:]
+        South = [0,:]
+        East = [:,-1]
+        West = [:,0]
+    """
+    edges = []
+
+    if boundary_mask.shape[1] > 2:
+        n0 = 1
+        n1 = -1
+    else:
+        n0 = 0
+        n1 = None
+
+    if np.any(boundary_mask[-1,n0:n1]):
+        edges.append('N')
+
+    if np.any(boundary_mask[0,n0:n1]):
+        edges.append('S')
+
+    if boundary_mask.shape[0] > 2:
+        n0 = 1
+        n1 = -1
+    else:
+        n0 = 0
+        n1 = None
+
+    if np.any(boundary_mask[n0:n1,-1]):
+        edges.append('E')
+
+    if np.any(boundary_mask[n0:n1,0]):
+        edges.append('W')
+
+    return edges
+
+def create_ordered_boundary_list(edge_list):
+    """Gets all edges, but given in a continuous clockwise direction.
+    If this is not possible (e.g. ['N','S']), then ampty list is returned."""
+    full_list = ['N', 'E', 'S', 'W']
+    for ind, edge in enumerate(full_list):
+        if edge not in edge_list:
+            full_list[ind] = ''
+    full_array = np.array(full_list)
+
+    ct = 0
+    while (np.where(full_array == '')[0][-1] != len(np.where(full_array == '')[0])-1) and ct < 5:
+        full_array = np.roll(full_array,1)
+        ct += 1
+
+    if ct > 4:
+        print(f'No continuous boundary can be found for edges {edge_list}. Returning empy list.')
+        return []
+
+    full_array = full_array[full_array != '']
+
+    return full_array.tolist()
+
+def get_coords_for_boundary_edges(edges: list, lon_edges: tuple[float, float], lat_edges: tuple[float, float]) -> tuple[np.ndarray, np.ndarray]:
+    """Create coordinate vectors for clockwise running edges.
+
+    Assumes that edges are clockwise and continuous, which is imposed by the
+    function create_ordered_boundary_list.
+
+    Empty list return empty arrays.
+    """
+    lon = []
+    lat = []
+    for edge in edges:
+        if edge == 'N':
+            lon.append(lon_edges[0])
+            lat.append(lat_edges[1])
+        if edge == 'S':
+            lon.append(lon_edges[1])
+            lat.append(lat_edges[0])
+        if edge == 'W':
+            lon.append(lon_edges[0])
+            lat.append(lat_edges[0])
+        if edge == 'E':
+            lon.append(lon_edges[1])
+            lat.append(lat_edges[1])
+
+    if edges:
+        edge = edges[-1] # Close the loop
+        if edge == 'N':
+            lon.append(lon_edges[1])
+            lat.append(lat_edges[1])
+        if edge == 'S':
+            lon.append(lon_edges[0])
+            lat.append(lat_edges[0])
+        if edge == 'W':
+            lon.append(lon_edges[0])
+            lat.append(lat_edges[1])
+        if edge == 'E':
+            lon.append(lon_edges[1])
+            lat.append(lat_edges[0])
+
+
+    return np.array(lon), np.array(lat)
+
+
+def create_swan_segment_coords(boundary_mask, lon_edges, lat_edges):
+    """Createsa longitude and latitude arrays for the SWAN BOUND SEGEMENT
+    command based on boundary mask.
+
+    Identifies edges (north, south etc.) and sets all the points on the edges
+    as boundary points.
+
+    If no continuous boundary can be identified, it returns empty list.
+    """
+
+    edge_list = identify_boundary_edges(boundary_mask)
+    clean_edge_list = create_ordered_boundary_list(edge_list)
+    lon, lat = get_coords_for_boundary_edges(clean_edge_list, lon_edges, lat_edges)
+    return lon, lat
---
+++ tests/unittests/utest_boundary_points.py
@@ -0,0 +1,186 @@
+import unittest
+from dnora.grd import Grid
+from dnora.grd.boundary import EdgesAsBoundary
+from dnora.aux_funcs import identify_boundary_edges, create_ordered_boundary_list, get_coords_for_boundary_edges, create_swan_segment_coords
+import numpy as np
+from copy import copy
+
+class IdentifyBnd(unittest.TestCase):
+	def test_all_edges(self):
+		grid = Grid(lon=(0,1), lat=(0,1))
+		grid.set_spacing(nx=4, ny =4)
+		grid.set_boundary(EdgesAsBoundary(edges=['N','W','S','E']))
+
+		self.assertEqual(set(identify_boundary_edges(grid.boundary_mask())),set(['N','W','S','E']))
+
+	def test_one_missing(self):
+		grid = Grid(lon=(0,1), lat=(0,1))
+		grid.set_spacing(nx=4, ny =5)
+
+		full_list = ['N','W','S','E']
+
+		for not_this in full_list:
+			incomplete_list = copy(full_list)
+			incomplete_list.remove(not_this)
+
+			grid.set_boundary(EdgesAsBoundary(edges=incomplete_list))
+			self.assertEqual(set(identify_boundary_edges(grid.boundary_mask())),set(incomplete_list))
+
+class OrderBnd(unittest.TestCase):
+	def test_order_full_list(self):
+		edges = ['S','E']
+		self.assertEqual(create_ordered_boundary_list(edges), ['E','S'])
+
+		edges = ['N','E']
+		self.assertEqual(create_ordered_boundary_list(edges), ['N','E'])
+
+		edges = ['W','E','S']
+		self.assertEqual(create_ordered_boundary_list(edges), ['E','S','W'])
+
+		edges = ['W','E','N']
+		self.assertEqual(create_ordered_boundary_list(edges), ['W','N','E'])
+
+		edges = ['W','E']
+		self.assertEqual(create_ordered_boundary_list(edges), [])
+
+
+class BoundaryCoordinates(unittest.TestCase):
+	def test_single_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		edges = ['N']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,3])), True)
+
+		edges = ['S']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,2])), True)
+
+		edges = ['W']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,3])), True)
+
+		edges = ['E']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,2])), True)
+
+
+	def test_two_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		edges = ['N','E']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,1,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,3,2])), True)
+
+		edges = ['S','W']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,0,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,2,3])), True)
+
+		edges = ['W','N']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,0,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,3,3])), True)
+
+		edges = ['E','S']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,1,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,2,2])), True)
+
+	def test_three_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		edges = ['N','E','S']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,1,1,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,3,2,2])), True)
+
+		edges = ['S','W','N']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,0,0,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,2,3,3])), True)
+
+		edges = ['W','N','E']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,0,1,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,3,3,2])), True)
+
+		edges = ['E','S','W']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([1,1,0,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,2,2,3])), True)
+
+
+	def test_four_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		edges = ['N','E','S','W']
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,1,1,0,0])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,3,2,2,3])), True)
+
+	def test_invalid_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		edges = []
+		lons, lats = get_coords_for_boundary_edges(edges, grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([])), True)
+		self.assertEqual(np.array_equal(lats,np.array([])), True)
+
+class FullPipeline(unittest.TestCase):
+	def test_single_edge(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		grid.set_boundary(EdgesAsBoundary(edges=['N']))
+		lons, lats = create_swan_segment_coords(grid.boundary_mask(), grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([3,3])), True)
+
+	def test_two_edges(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		grid.set_boundary(EdgesAsBoundary(edges=['N','W']))
+		lons, lats = create_swan_segment_coords(grid.boundary_mask(), grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,0,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,3,3])), True)
+
+	def test_three_edges(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		grid.set_boundary(EdgesAsBoundary(edges=['N','W','E']))
+		lons, lats = create_swan_segment_coords(grid.boundary_mask(), grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([0,0,1,1])), True)
+		self.assertEqual(np.array_equal(lats,np.array([2,3,3,2])), True)
+
+	def test_no_edges(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+
+		lons, lats = create_swan_segment_coords(grid.boundary_mask(), grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([])), True)
+		self.assertEqual(np.array_equal(lats,np.array([])), True)
+
+	def test_invalid_edges(self):
+		grid = Grid(lon=(0,1), lat=(2,3))
+		grid.set_spacing(nx=4, ny =5)
+		grid.set_boundary(EdgesAsBoundary(edges=['W','E']))
+
+		lons, lats = create_swan_segment_coords(grid.boundary_mask(), grid.lon_edges(), grid.lat_edges())
+		self.assertEqual(np.array_equal(lons,np.array([])), True)
+		self.assertEqual(np.array_equal(lats,np.array([])), True)
+
+if __name__ == '__main__':
+	unittest.main()
