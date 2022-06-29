a3a5cf2778791804f51e30d0562311d6d6c47bf0
Jan-Victor Björkqvist <janvb@met.no>
2022-06-29 20:48:36 +0200

    Unstructured grid can now be initialized based on a structured grid

--- dnora/skeletons/unstr_grd_mod.py
+++ dnora/skeletons/unstr_grd_mod.py
@@ -6,7 +6,10 @@ import xarray as xr
 from dnora import msg
 
 class UnstrGrid(PointSkeleton, Topography):
-    def __init__(self, x=None, y=None, lon=None, lat=None, name='AnonymousGrid'):
+    def __init__(self, grid=None, x=None, y=None, lon=None, lat=None, name='AnonymousGrid'):
         self.name = name
+        if grid is not None:
+            x, y = grid.xy(strict=True)
+            lon, lat = grid.lonlat(strict=True)
         self.data = self._create_structure(x, y, lon, lat)
         self._reset_vars()
