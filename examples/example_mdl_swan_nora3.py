dfc37db749a1a873cda3d3bb706b223930188571
KonstantinChri <konstantinosc@met.no>
2022-07-01 01:11:48 +0200

    update example

--- examples/example_mdl_swan_nora3.py
+++ examples/example_mdl_swan_nora3.py
@@ -1,7 +1,7 @@
 # =============================================================================
 # IMPORT dnora
 # =============================================================================
-from dnora import grd, mdl, wnd, bnd
+from dnora import grd, mdl, wnd, bnd, inp
 # =============================================================================
 # DEFINE GRID OBJECT
 # =============================================================================
@@ -9,33 +9,32 @@ from dnora import grd, mdl, wnd, bnd
 grid = grd.Grid(lon=(4.00, 5.73), lat=(60.53, 61.25), name='Skjerjehamn250')
 
 # Set spacing and boundary points
-#grid.set_spacing(dm=250)
+grid.set_spacing(dm=250)
 
 # Import topography and mesh it down to the grid definitions
-#topo_reader=grd.read.EMODNET2020(tile='*',folder='/home/janvb/Documents/EMODNET2020/')
-#grid.import_topo(topo_reader=topo_reader)
+topo_reader=grd.read.EMODNET2020(tile='*',folder='/home/konstantinosc/bathy')
+grid.import_topo(topo_reader=topo_reader)
 
 # This can be used to get an empty topography for testing
 #grid.import_topo(topo_reader=grd.read.EmptyTopo(grid=grid))
 #
-#grid.mesh_grid()
+grid.mesh_grid()
 #
 # Set the boundaries
-bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'], step=20)
+bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S'])
 grid.set_boundary(boundary_setter=bnd_set)
 #
 #
 # =============================================================================
 # DEFINE MODEL OBJECT
 # =============================================================================
-model = mdl.SWAN_NORA3(grid, start_time='2018-08-25T00:00',
-                              end_time='2018-08-25T03:00')
+model = mdl.SWAN_NORA3(grid, start_time='2021-06-25T00:00',
+                              end_time='2021-06-25T03:00')
 # =============================================================================
 # IMPORT BOUNDARIES AND FORCING
 # =============================================================================
-model.import_boundary(bnd.read_metno.NORA3(source='lustre'), write_cache=True, read_cache=False)
+model.import_boundary(bnd.read_metno.NORA3(source='thredds'), write_cache=True, read_cache=False)
 model.import_forcing(wnd.read_metno.NORA3(source='thredds'), write_cache=True, read_cache=False)
-breakpoint()
 # =============================================================================
 # PLOT GRID, FORCING AND BOUNDARIES
 # =============================================================================
@@ -46,7 +45,7 @@ model.plot_grid(save_fig=True, show_fig=False)
 model.export_grid()
 model.export_boundary()
 model.export_forcing()
-model.write_input_file()
+model.write_input_file(input_file_writer=inp.SWAN(spec_points=[(4.4, 60.6),(4.4, 60.8)]))
 # =============================================================================
 # SWAN RUN
 # =============================================================================
