8d8a328d5ce80d2b061865ff112ac4387b61d841
KonstantinChri <kchrist87@gmail.com>
2023-01-11 15:09:29 +0100

    remove/add example scripts

--- examples/example_mdl_swan.py
+++ examples/example_mdl_swan.py
@@ -1,19 +1,20 @@
 # =============================================================================
 # IMPORT dnora
 # =============================================================================
-from dnora import grd, mdl, wnd, bnd, inp
+from dnora import grd, mdl, wnd, bnd, inp, wlv, ocr, run
 # =============================================================================
 # DEFINE GRID OBJECT
 # =============================================================================
 # Set grid definitions
-grid = grd.Grid(lon=(3.0, 3.5), lat=(56.3, 56.9), name='Ekofisk250')
+grid = grd.Grid(lon=(5.35, 5.6), lat=(59.00, 59.17), name='Boknafjorden')
 
 # Set spacing and boundary points
 grid.set_spacing(dm=250)
 
 # Import topography and mesh it down to the grid definitions
+# Options for grd.readers: EMODNET2020, KartverketNo50m, GEBCO2021
 topo_reader = grd.read.EMODNET2020(
-    tile='*', folder='/home/konstac/bathy')
+    tile='*5', folder='/home/konstac/bathy')
 grid.import_topo(topo_reader=topo_reader)
 
 # This can be used to get an empty topography for testing
@@ -22,21 +23,32 @@ grid.import_topo(topo_reader=topo_reader)
 grid.mesh_grid()
 #
 # Set the boundaries
-bnd_set = grd.boundary.EdgesAsBoundary(edges=['N', 'W', 'S', 'E'])
+bnd_set = grd.boundary.EdgesAsBoundary(edges=[ 'W', 'S'], step = 10)
 grid.set_boundary(boundary_setter=bnd_set)
 #
 #
 # =============================================================================
-# DEFINE MODEL OBJECT
+# DEFINE MODEL OBJECT (mdl.)
 # =============================================================================
-model = mdl.SWAN_NORA3(grid, start_time='2018-01-23T18:00',
-                       end_time='2018-01-23T20:00')
+# Options for mdl: SWAN_NORA3, SWAN_ERA5, SWAN_WW3_4km, SWAN_WAM4km
+model = mdl.SWAN_NORA3(grid, start_time='2020-12-17T12:00',
+                               end_time='2020-12-17T18:00')
 # =============================================================================
 # IMPORT BOUNDARIES AND FORCING
 # =============================================================================
-model.import_boundary(bnd.read_metno.NORA3(), write_cache=True, read_cache=False)
-model.import_forcing(wnd.read_metno.NORA3(program='pyfimex'),
-                     write_cache=True, read_cache=False)
+# Boundary Spectra
+model.import_boundary(write_cache=True, read_cache=False)
+#model.import_boundary(bnd.read_metno.NORA3(source='lustre'),write_cache=True, read_cache=False) # for internal
+
+# Wind Forcing
+model.import_forcing(write_cache=True, read_cache=False)
+#model.import_forcing(wnd.read_metno.NORA3(source='lustre'),write_cache=True, read_cache=False)  # for internal
+
+# Ocean Current
+#model.import_oceancurrent(ocr.read_metno.NorKyst800())
+
+# Water Level
+#model.import_waterlevel(wlv.read_ec.GTSM_ERA5())
 # =============================================================================
 # PLOT GRID, FORCING AND BOUNDARIES
 # =============================================================================
@@ -47,9 +59,11 @@ model.plot_grid(save_fig=True, show_fig=False)
 model.export_grid()
 model.export_boundary()
 model.export_forcing()
+#model.export_oceancurrent()
+#model.export_waterlevel()
 model.write_input_file(input_file_writer=inp.SWAN(
-    spec_points=[(3.209986, 56.549197)]))
+    spec_points=[(5.50, 59.16), (5.55, 59.15)]))
 # =============================================================================
 # SWAN RUN
 # =============================================================================
-model.run_model()
+model.run_model(model_executer = run.SWAN(nproc=15))
