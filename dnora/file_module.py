6f6dd70515c7dd8520d3d1f0889676375c46202a
Jan-Victor Björkqvist <janvb@met.no>
2024-03-04 20:04:42 +0100

    Added folder dateformat

--- dnora/file_module.py
+++ dnora/file_module.py
@@ -56,27 +56,37 @@ class FileNames:
             "dateformat", self.obj_type, self.primary, self.fallback
         )
 
+    def get_dateformat_folder(self) -> str:
+        return self.dateformat or get_default_value(
+            "dateformat_folder", self.obj_type, self.primary, self.fallback
+        )
+
     def get_filename(
         self,
         extension: str = None,
         start_time: str = None,
         end_time: str = None,
-        key: str = "filename",
+        lon: tuple[float, float] = None,
+        lat: tuple[float, float] = None,
+        x: tuple[float, float] = None,
+        y: tuple[float, float] = None,
         clean: bool = True,
-        edge_object: DnoraDataType = None,
     ) -> str:
         if self.filename is not None:
             filename = self.filename
         else:
             filename = get_default_value(
-                key, self.obj_type, self.primary, self.fallback
+                "filename", self.obj_type, self.primary, self.fallback
             )
         start_time = start_time or self.get_start_time()
         end_time = end_time or self.get_end_time()
-        edge_object = edge_object or self.edge_object
+        lon = lon or self.model[self.edge_object].edges("lon", strict=True)
+        lat = lat or self.model[self.edge_object].edges("lat", strict=True)
+        x = x or self.model[self.edge_object].edges("x", strict=True)
+        y = y or self.model[self.edge_object].edges("y", strict=True)
 
         filename = self.replace_placeholders(
-            filename, start_time, end_time, edge_object=edge_object
+            filename, start_time, end_time, lon, lat, x, y
         )
 
         extension = extension or get_default_value(
@@ -91,12 +101,30 @@ class FileNames:
         return f"{Path(filename)}.{extension}"
 
     def get_folder(
-        self, key: str = "folder", clean: bool = True, edge_object: str = None
+        self,
+        start_time: str = None,
+        end_time: str = None,
+        lon: tuple[float, float] = None,
+        lat: tuple[float, float] = None,
+        x: tuple[float, float] = None,
+        y: tuple[float, float] = None,
+        clean: bool = True,
     ) -> str:
         folder = self.folder or get_default_value(
-            key, self.obj_type, self.primary, self.fallback
+            "folder", self.obj_type, self.primary, self.fallback
         )
-        folder = str(Path(self.replace_placeholders(folder, edge_object=edge_object)))
+
+        start_time = start_time or self.get_start_time()
+        end_time = end_time or self.get_end_time()
+        lon = lon or self.model[self.edge_object].edges("lon", strict=True)
+        lat = lat or self.model[self.edge_object].edges("lat", strict=True)
+        x = x or self.model[self.edge_object].edges("x", strict=True)
+        y = y or self.model[self.edge_object].edges("y", strict=True)
+        folder = str(
+            Path(
+                self.replace_placeholders(folder, start_time, end_time, lon, lat, x, y)
+            )
+        )
 
         if clean:
             folder = clean_filename(folder)
@@ -107,24 +135,30 @@ class FileNames:
         extension: str = None,
         start_time: str = None,
         end_time: str = None,
-        key: str = "filename",
+        lon: tuple[float, float] = None,
+        lat: tuple[float, float] = None,
+        x: tuple[float, float] = None,
+        y: tuple[float, float] = None,
         clean: bool = True,
-        edge_object: str = None,
     ) -> str:
         return add_folder_to_filename(
             self.get_filename(
                 extension,
                 start_time,
                 end_time,
-                edge_object=edge_object,
-                key=key,
+                lon=lon,
+                lat=lat,
+                x=x,
+                y=y,
                 clean=clean,
             ),
-            self.get_folder(edge_object=edge_object, clean=clean),
+            self.get_folder(
+                start_time, end_time, lon=lon, lat=lat, x=x, y=y, clean=clean
+            ),
         )
 
-    def create_folder(self, key: str = "folder", edge_object: str = None) -> None:
-        folder = Path(self.get_folder(key=key, edge_object=edge_object))
+    def create_folder(self, edge_object: str = None) -> None:
+        folder = Path(self.get_folder())
 
         if not folder.is_dir():
             msg.plain(f"Creating folder {str(folder)}")
@@ -135,7 +169,10 @@ class FileNames:
         unclean_string: str,
         start_time: str = None,
         end_time: str = None,
-        edge_object: DnoraDataType = None,
+        lon: tuple[float, float] = None,
+        lat: tuple[float, float] = None,
+        x: tuple[float, float] = None,
+        y: tuple[float, float] = None,
     ) -> str:
 
         unclean_string = replace_object_type_name(unclean_string, self.obj_name)
@@ -147,18 +184,20 @@ class FileNames:
         )
         unclean_string = replace_object_type(unclean_string, self.obj_type)
 
-        edge_object = self.model[edge_object or self.edge_object]
-
-        lon = edge_object.edges("lon", strict=True)
-        lat = edge_object.edges("lat", strict=True)
-        x = edge_object.edges("x", strict=True)
-        y = edge_object.edges("y", strict=True)
-
-        unclean_string = replace_lonlat(unclean_string, lon, lat)
-        unclean_string = replace_xy(unclean_string, x, y)
+        fmt = get_default_value(
+            "coord_format", self.obj_type, self.primary, self.fallback
+        )
+        cart_fmt = get_default_value(
+            "cartesian_coord_format", self.obj_type, self.primary, self.fallback
+        )
+        unclean_string = replace_lonlat(unclean_string, lon, lat, fmt)
+        unclean_string = replace_xy(unclean_string, x, y, cart_fmt)
 
         clean_string = replace_times(
-            unclean_string, self.get_dateformat(), [start_time, end_time]
+            unclean_string,
+            self.get_dateformat(),
+            self.get_dateformat_folder(),
+            [start_time, end_time],
         )
 
         return clean_string
@@ -202,7 +241,9 @@ def add_suffix(filename: str, suffix: str) -> str:
         return f"{filename}_{suffix}.{extension}"
 
 
-def replace_times(filename: str, dateformat: str, times: list) -> str:
+def replace_times(
+    filename: str, dateformat: str, dateformat_folder: str, times: list
+) -> str:
     """Substitutes the strings #T0, #T1, #T2... etc. in filname with time
     stamps from a list of times, using the format in dateformat.
 
@@ -212,11 +253,14 @@ def replace_times(filename: str, dateformat: str, times: list) -> str:
     for ct, t in enumerate(times):
         if t is not None:
             filename = re.sub(f"#T{ct}", pd.Timestamp(t).strftime(dateformat), filename)
+            filename = re.sub(
+                f"#FT{ct}", pd.Timestamp(t).strftime(dateformat_folder), filename
+            )
 
     return filename
 
 
-def replace_lonlat(filename: str, lon: float, lat: float) -> str:
+def replace_lonlat(filename: str, lon: float, lat: float, fmt: str) -> str:
     """Substitutes the strings #Lon, #Lat in filename with values of lon and
     lat.
 
@@ -225,26 +269,26 @@ def replace_lonlat(filename: str, lon: float, lat: float) -> str:
 
     if isinstance(lon, tuple):
         if lon[0] is not None:
-            filename = re.sub("#Lon0", f"{lon[0]:010.7f}", filename)
+            filename = re.sub("#LON0", f"{lon[0]:{fmt}}", filename)
         if lon[1] is not None:
-            filename = re.sub("#Lon1", f"{lon[1]:010.7f}", filename)
+            filename = re.sub("#LAT1", f"{lon[1]:{fmt}}", filename)
     else:
         if lon is not None:
-            filename = re.sub("#Lon", f"{lon:010.7f}", filename)
+            filename = re.sub("#LON", f"{lon:{fmt}}", filename)
 
     if isinstance(lat, tuple):
         if lat[0] is not None:
-            filename = re.sub("#Lat0", f"{lat[0]:010.7f}", filename)
+            filename = re.sub("#LAT0", f"{lat[0]:{fmt}}", filename)
         if lat[1] is not None:
-            filename = re.sub("#Lat1", f"{lat[1]:010.7f}", filename)
+            filename = re.sub("#LAT1", f"{lat[1]:{fmt}}", filename)
     else:
         if lon is not None:
-            filename = re.sub("#Lat", f"{lat:010.7f}", filename)
+            filename = re.sub("#LAT", f"{lat:{fmt}}", filename)
 
     return filename
 
 
-def replace_xy(filename: str, x: float, y: float) -> str:
+def replace_xy(filename: str, x: float, y: float, fmt: str) -> str:
     """Substitutes the strings #X, #Y in filename with values of lon and
     lat.
 
@@ -252,21 +296,21 @@ def replace_xy(filename: str, x: float, y: float) -> str:
     """
     if isinstance(x, tuple):
         if x[0] is not None:
-            filename = re.sub("#X0", f"{x[0]:010.3f}", filename)
+            filename = re.sub("#X0", f"{x[0]:{fmt}}", filename)
         if x[1] is not None:
-            filename = re.sub("#Y1", f"{x[1]:010.3f}", filename)
+            filename = re.sub("#Y1", f"{x[1]:{fmt}}", filename)
     else:
         if x is not None:
-            filename = re.sub("#X", f"{x:010.3f}", filename)
+            filename = re.sub("#X", f"{x:{fmt}}", filename)
 
     if isinstance(y, tuple):
         if y[0] is not None:
-            filename = re.sub("#Y0", f"{y[0]:010.3f}", filename)
+            filename = re.sub("#Y0", f"{y[0]:{fmt}}", filename)
         if y[1] is not None:
-            filename = re.sub("#Y1", f"{y[1]:010.3f}", filename)
+            filename = re.sub("#Y1", f"{y[1]:{fmt}}", filename)
     else:
         if y is not None:
-            filename = re.sub("#Y", f"{y:010.3f}", filename)
+            filename = re.sub("#Y", f"{y:{fmt}}", filename)
 
     return filename
 
