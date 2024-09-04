f1c6f3c1f6b1db3957c2f264688552cb05ce5002
Jan-Victor Björkqvist <jvbjorkq@gmail.com>
2024-09-05 00:23:55 +0200

    Updated to new geo-skeleton feature of static core

--- dnora/waveseries/waveseries.py
+++ dnora/waveseries/waveseries.py
@@ -1,9 +1,9 @@
 from geo_skeletons import PointSkeleton
-from geo_skeletons.decorators import add_mask, add_time
+from geo_skeletons.decorators import add_mask, add_time, dynamic
 
 from copy import deepcopy
 
-
+@dynamic
 @add_mask(name="buoy", coord_group="grid", default_value=1)
 @add_time(grid_coord=False)
 class WaveSeries(PointSkeleton):
