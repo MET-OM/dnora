5f0ce911068f94530d27d71525f539efc2d1bd1e
Jan-Victor Björkqvist <janvb@met.no>
2024-09-19 14:39:37 +0200

    fixed import

--- dnora/grid/templates.py
+++ dnora/grid/templates.py
@@ -1,6 +1,6 @@
 from dnora.grid import Grid
 from dnora.read.grid.grid_readers import EMODNET, GEBCO
-
+from dnora.read.grid.nchmf import DepFile
 
 class EMODNET(Grid):
     _default_reader = EMODNET()
@@ -10,4 +10,4 @@ class GEBCO(Grid):
     _default_reader = GEBCO()
 
 class NCHMF(Grid):
-    _default_reader = dnora.read.grid.grid_readers.nchmf.DepFile()
+    _default_reader = DepFile()
