8b6027dc21e0a60aa11e3207e6450058513e2cb1
Jan-Victor Björkqvist <janvb@met.no>
2025-02-26 13:09:18 +0100

    fixe obj_type issue

--- dnora/export/decorators.py
+++ dnora/export/decorators.py
@@ -4,7 +4,7 @@ from dnora.type_manager.dnora_types import DnoraDataType
 def add_export_method(obj_type: DnoraDataType):
     def wrapper(c):
         def export(self, *args, **kwargs) -> None:
-            self.export(obj_type=obj_type, *args, **kwargs)
+            self.export(obj_type, *args, **kwargs)
 
         exec(f"c.export_{obj_type.name.lower()} = export")
         return c
