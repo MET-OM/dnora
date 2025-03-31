9a577eb8d1b57880cda279e5a69fcebf4fbd2657
Jan-Victor Björkqvist <janvb@met.no>
2025-03-31 15:33:50 +0200

    fixed deffault folder name

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -6,26 +6,26 @@ import shutil
 
 
 def test_ww3_spectral_export_one_file():
-    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12))
+    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12),name='TestGrid')
     grid.set_boundary_points(dn.grid.mask.All())
     model = dn.modelrun.Constant(grid, year=2019, month=1, day=1)
     model.import_spectra()
     exporter = dn.export.WW3(model)
     exporter.export_spectra(filename="pytest_ww3_spectra")
 
-    ds = xr.open_dataset("output/pytest_ww3_spectra.nc")
+    ds = xr.open_dataset("TestGrid_WW3/pytest_ww3_spectra.nc")
     np.testing.assert_array_almost_equal(
         np.full((24, 3), [0, 1, 2]), ds.longitude.values
     )
     np.testing.assert_array_almost_equal(
         np.full((24, 3), [10, 11, 12]), ds.latitude.values
     )
-    if os.path.isdir("output"):
-        shutil.rmtree("output")
+    if os.path.isdir("TestGrid_WW3"):
+        shutil.rmtree("TestGrid_WW3")
 
 
 def test_ww3_spectral_export_three_files():
-    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12))
+    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12),name='TestGrid')
     grid.set_boundary_points(dn.grid.mask.All())
     model = dn.modelrun.Constant(grid, year=2019, month=1, day=1)
     model.import_spectra()
@@ -35,27 +35,27 @@ def test_ww3_spectral_export_three_files():
         filename="pytest_ww3_spectra_#LON0_#LAT0",
     )
     for lon, lat in zip([0, 1, 2], [10, 11, 12]):
-        ds = xr.open_dataset(f"output/pytest_ww3_spectra_{lon:010.7f}_{lat:010.7f}.nc")
+        ds = xr.open_dataset(f"TestGrid_WW3/pytest_ww3_spectra_{lon:010.7f}_{lat:010.7f}.nc")
         np.testing.assert_array_almost_equal(
             np.full((24, 1), [lon]), ds.longitude.values
         )
         np.testing.assert_array_almost_equal(
             np.full((24, 1), [lat]), ds.latitude.values
         )
-    if os.path.isdir("output"):
-        shutil.rmtree("output")
+    if os.path.isdir("TestGrid_WW3"):
+        shutil.rmtree("TestGrid_WW3")
 
 
 def test_ww3_spectral_export_squeeze_lonlat():
-    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12))
+    grid = dn.grid.TriGrid(lon=(0, 1, 2), lat=(10, 11, 12), name='TestGrid')
     grid.set_boundary_points(dn.grid.mask.All())
     model = dn.modelrun.Constant(grid, year=2019, month=1, day=1)
     model.import_spectra()
     exporter = dn.export.WW3(model)
     exporter.export_spectra(squeeze_lonlat=True, filename="pytest_ww3_spectra")
 
-    ds = xr.open_dataset("output/pytest_ww3_spectra.nc")
+    ds = xr.open_dataset("TestGrid_WW3/pytest_ww3_spectra.nc")
     np.testing.assert_array_almost_equal([0, 1, 2], ds.longitude.values)
     np.testing.assert_array_almost_equal([10, 11, 12], ds.latitude.values)
-    if os.path.isdir("output"):
-        shutil.rmtree("output")
+    if os.path.isdir("TestGrid_WW3"):
+        shutil.rmtree("TestGrid_WW3")
