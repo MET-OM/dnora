35ba11316cee15a9a5913e8b98e75b2d4ac5c8fa
Jan-Victor Björkqvist <janvb@met.no>
2025-04-02 14:03:57 +0200

    added cleanup

--- tests/test_remote_waterlevel_readers.py
+++ tests/test_remote_waterlevel_readers.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 from dnora.read.wind.metno import get_meps_urls, MEPS
 from dnora.type_manager.data_sources import DataSource
-
+import os, shutil
 
 def grid_is_covered(grid, skeleton):
     assert skeleton.lon()[0] < grid.lon()[0]
@@ -15,20 +15,25 @@ def grid_is_covered(grid, skeleton):
 
 @pytest.fixture(scope="session")
 def grid():
-    return dn.grid.Grid(lon=(10, 14), lat=(60, 61))
+    return dn.grid.Grid(lon=(4, 6), lat=(58, 61))
 
 
 @pytest.fixture(scope="session")
 def timevec():
     return pd.date_range("2022-04-01 00:00:00", "2022-04-01 23:00:00", freq="1h")
+def cleanup():
+    if os.path.isdir("dnora_waterlevel_temp"):
+        shutil.rmtree("dnora_waterlevel_temp")
 
 
 @pytest.mark.slow
 @pytest.mark.remote
 def test_era5(grid, timevec):
+    cleanup()
     model = dn.modelrun.ModelRun(grid, year=2022, month=4, day=1)
     model.import_waterlevel(dn.read.waterlevel.ec.GTSM_ERA5())
 
     assert np.all(model.waterlevel().time() == timevec)
 
     grid_is_covered(grid, model.waterlevel())
+    cleanup()
