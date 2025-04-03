ba87906c3db6ea1a82f04aa65b178e5bd5c48937
Jan-Victor Björkqvist <janvb@met.no>
2025-04-03 13:03:17 +0200

    added decorator for run methods

--- dnora/executer/decorators.py
+++ dnora/executer/decorators.py
@@ -4,9 +4,19 @@ from dnora.type_manager.dnora_types import DnoraFileType
 def add_write_method(file_type: DnoraFileType):
     def wrapper(c):
         def write(self, **kwargs) -> None:
-            self.write(file_type=file_type, **kwargs)
+            self._write(file_type=file_type, **kwargs)
 
         exec(f"c.write_{file_type.name.lower()}_file = write")
+        return c
+
+    return wrapper
+
+def add_run_method(file_type: DnoraFileType):
+    def wrapper(c):
+        def run(self, **kwargs) -> None:
+            self._run(file_type=file_type, **kwargs)
+
+        exec(f"c.run_{file_type.name.lower()} = run")
         return c
 
     return wrapper
