11cb533d49108902c60e7d93f7b00bf61af19f99
Jan-Victor Björkqvist <janvb@met.no>
2025-05-08 15:01:07 +0200

    added post-processor

--- dnora/executer/post_processors.py
+++ dnora/executer/post_processors.py
@@ -4,6 +4,26 @@ from dnora.file_module import FileNames
 import scipy
 import numpy as np
 import xarray as xr
+import geo_parameters as gp
+from geo_skeletons import GriddedSkeleton
+from dnora import msg
+import pathlib
+
+
+SWAN_VARS = {
+    "Hsig": gp.wave.Hs,
+    "Tm01": gp.wave.Tm01,
+    "Tm02": gp.wave.Tm02,
+    "Tm_10": gp.wave.Tm_10,
+    "Depth": gp.ocean.WaterDepth,
+    "Windv_x": gp.wind.XWind,
+    "Windv_y": gp.wind.YWind,
+    "Dspr": gp.wave.Spr,
+    "Dir": gp.wave.Dirm,
+    "TPsmoo": gp.wave.Tp,
+    "RTpeak": gp.wave.Tp,
+    "PkDir": gp.wave.Dirp,
+}
 
 
 class PostProcessor(ABC):
@@ -14,6 +34,69 @@ class PostProcessor(ABC):
     @abstractmethod
     def __str__(self) -> str:
         pass
+
+
+def read_swan_mat_to_ds(filename: str, lon: np.ndarray, lat=np.ndarray) -> xr.Dataset:
+    """Reads a SWAN mat-file and creates an xarray Dataset with standard names"""
+    # This creates a dict with keys
+    # 'Hsig_20200215_000000', 'RTpeak_20200215_000000', 'TPsmoo_20200215_000000' ...
+    mat = scipy.io.loadmat(filename)
+
+    # Create class
+    wave_grid = GriddedSkeleton.add_time()
+    for key, value in SWAN_VARS.items():
+        wave_grid = wave_grid.add_datavar(value(key))
+
+    key = list(SWAN_VARS.keys())[0]
+    keys = [a for a in mat.keys() if key == a.split("_")[0]]
+    keys.sort()
+    start_time = "T".join(keys[0].split("_")[1:])
+    end_time = "T".join(keys[-1].split("_")[1:])
+
+    obj = wave_grid(lon=lon, lat=lat, time=(start_time, end_time))
+
+    for swan_var, param in SWAN_VARS.items():
+
+        keys = [a for a in mat.keys() if swan_var == "_".join(a.split("_")[0:-2])]
+
+        keys.sort()
+        if keys:
+            msg.plain(f"Decoding variable {swan_var} as {param}...")
+        for n, kk in enumerate(keys):
+            obj.ind_insert(
+                swan_var,
+                np.where(np.isnan(mat.get(kk)), np.nan, mat.get(kk)),
+                time=n,
+            )
+
+    return obj.ds()
+
+
+class SwanMatToNc(PostProcessor):
+    def __init__(self, file_names_to_convert: list[str]):
+        self._file_names_to_convert = file_names_to_convert
+
+    def __call__(self, model: ModelRun, file_object: FileNames, **kwargs) -> None:
+
+        lon = model.grid().lon()
+        lat = model.grid().lat()
+
+        for filename in self._file_names_to_convert:
+            filename = pathlib.Path(filename.replace("'", "")).stem
+
+            input_file = f"{file_object.get_folder()}/{filename}.mat"
+            output_file = f"{file_object.get_folder()}/{filename}.nc"
+
+            msg.from_file(input_file)
+            msg.blank()
+            ds = read_swan_mat_to_ds(input_file, lon=lon, lat=lat)
+
+            msg.blank()
+            msg.to_file(output_file)
+            ds.to_netcdf(output_file)
+
+    def __str__(self):
+        return "Converting SWAN output mat-file to NetCDF..."
 
 
 class SwashMatToNc(PostProcessor):
