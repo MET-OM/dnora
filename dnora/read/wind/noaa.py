87e0b79cf77f430c4945b2f6fe4fed5df5c1a174
Jan-Victor Björkqvist <janvb@met.no>
2025-05-14 13:07:02 +0200

    added readers

--- dnora/read/wind/noaa.py
+++ dnora/read/wind/noaa.py
@@ -14,114 +14,75 @@ from dnora.aux_funcs import get_url
 
 from dnora.type_manager.data_sources import DataSource
 from dnora.read.abstract_readers import DataReader
+from dnora.read.product_readers import ProductReader
+from dnora.read.product_configuration import ProductConfiguration
+from dnora.read.file_structure import FileStructure
+from dnora.read.ds_read_functions import basic_xarray_read
+from functools import partial
+import geo_parameters as gp
 
 
-class GFS(DataReader):
-    """Reads wind data of the GFS global forecast"""
+class PacIOOS(ProductReader):
+    """Reads GFS wind data from the best estimate files. 2h data for 5 days.
 
-    def default_data_source(self) -> DataSource:
-        return DataSource.REMOTE
+    Creator: NOAA National Centers for Environmental Prediction (NCEP)
+    Publisher: Pacific Islands Ocean Observing System (PacIOOS)
 
-    def __init__(
-        self,
-        stride: int = 6,
-        hours_per_file: int = 121,
-        last_file: str = "",
-        lead_time: int = 0,
-    ):
-        """The data is currently in hourly files. Do not change the default
-        setting unless you have a good reason to do so.
-        """
+    U.S. National Oceanic and Atmospheric Administration (NOAA) National Centers for Environmental Prediction (NCEP) Global Forecast System (GFS) numerical weather prediction model 8-day, 3-hourly global forecast at approximately 50-km or 0.5-deg
+    """
 
-        self.stride = stride
-        self.hours_per_file = hours_per_file
-        self.lead_time = lead_time
-        self.last_file = last_file
+    product_configuration = ProductConfiguration(
+        filename="NCEP_Global_Atmospheric_Model_best.ncd",
+        default_folders={
+            DataSource.REMOTE: "https://pae-paha.pacioos.hawaii.edu/thredds/dodsC/ncep_global/",
+        },
+        data_vars=["ugrd10m", "vgrd10m"],
+        ds_aliases={"ugrd10m": gp.wind.XWind, "vgrd10m": gp.wind.YWind},
+        default_data_source=DataSource.REMOTE,
+    )
 
-    def _folder_filename(
-        self, source: DataSource, folder: str, filename: str
-    ) -> tuple[str]:
-        if source == DataSource.REMOTE:
-            folder = "http://nomads.ncep.noaa.gov:80/dods/gfs_0p25_1hr/gfs%Y%m%d"
-        if filename is None:
-            filename = "gfs_0p25_1hr_%Hz"
-        return folder, filename
+    file_structure = FileStructure()
 
-    def __call__(
-        self,
-        grid: Grid,
-        start_time: str,
-        end_time: str,
-        source: DataSource,
-        folder: str,
-        expansion_factor: float = 1.2,
-        **kwargs,
-    ):
-        """Reads wind data between given times and given area around
-        the Grid object."""
 
-        self.start_time = start_time
-        self.end_time = end_time
+class NCEP(ProductReader):
+    """Reads GFS wind data from the NCEP thredds server
 
-        start_times, end_times, file_times = utils.time.create_time_stamps(
-            start_time,
-            end_time,
-            self.stride,
-            self.hours_per_file,
-            self.last_file,
-            self.lead_time,
-        )
+    3h data for 16 days
+    """
 
-        msg.info(
-            f"Getting wind forcing from GFS from {self.start_time} to {self.end_time}"
-        )
-        msg.info(f"Using expansion_factor = {expansion_factor:.2f}")
-        # Define area to search in
-        lon, lat = utils.grid.expand_area(
-            grid.edges("lon"), grid.edges("lat"), expansion_factor
-        )
+    product_configuration = ProductConfiguration(
+        filename="gfs_0p25_%Hz",
+        default_folders={
+            DataSource.REMOTE: "https://nomads.ncep.noaa.gov/dods/gfs_0p25/gfs%Y%m%d/",
+        },
+        ds_creator_function=partial(
+            basic_xarray_read, time_reference_str="days since 0001-01-01 00:00:0.0"
+        ),
+        data_vars=["ugrd10m", "vgrd10m"],
+        ds_aliases={"ugrd10m": gp.wind.XWind, "vgrd10m": gp.wind.YWind},
+        default_data_source=DataSource.REMOTE,
+    )
 
-        wnd_list = []
-        for n in range(len(file_times)):
-            folder, filename = self._folder_filename(source, folder, filename=None)
-            url = get_url(folder, filename, file_times[n])
-            msg.plain(f"Reading wind forcing data: {start_times[n]}-{end_times[n]}")
-            msg.from_file(url)
+    file_structure = FileStructure(stride=6, hours_per_file=385)
 
-            # with dask.config.set(**{"array.slicing.split_large_chunks": True}):
-            #     with xr.open_dataset(url, decode_times=False) as ds:
 
-            ds = xr.open_dataset(url, decode_times=False)
+class NCEP1h(ProductReader):
+    """Reads GFS wind data from the NCEP thredds server
 
-            t0 = pd.to_datetime(
-                ds.time.minimum[-4:] + " " + ds.time.minimum[3:8]
-            ) + timedelta(hours=int(ds.time.minimum[:2]))
-            t1 = pd.to_datetime(
-                ds.time.maximum[-4:] + " " + ds.time.maximum[3:8]
-            ) + timedelta(hours=int(ds.time.maximum[:2]))
+    1h data for 5 days
+    """
 
-            # ds["time"] = ds.time.dt.round("H")
-            ds["time"] = pd.date_range(t0, t1, freq="1h")
+    product_configuration = ProductConfiguration(
+        filename="gfs_0p25_1hr_%Hz",
+        default_folders={
+            DataSource.REMOTE: "https://nomads.ncep.noaa.gov/dods/gfs_0p25_1hr/gfs%Y%m%d/",
+        },
+        ds_creator_function=partial(
+            basic_xarray_read, time_reference_str="days since 0001-01-01 00:00:0.0"
+        ),
+        data_vars=["ugrd10m", "vgrd10m"],
+        ds_aliases={"ugrd10m": gp.wind.XWind, "vgrd10m": gp.wind.YWind},
+        default_data_source=DataSource.REMOTE,
+    )
 
-            ds = ds.sel(
-                time=slice(start_times[n], end_times[n]),
-                lon=slice(lon[0], lon[1]),
-                lat=slice(lat[0], lat[1]),
-            )[["lon", "lat", "time", "ugrd10m", "vgrd10m"]]
-            wnd_list.append(ds)
-
-        wind_forcing = xr.concat(wnd_list, dim="time")
-
-        u = wind_forcing.ugrd10m.data
-        v = wind_forcing.vgrd10m.data
-        # u = np.moveaxis(u, 0, 2)
-        # v = np.moveaxis(v, 0, 2)
-        data_dict = {"u": u, "v": v}
-        coord_dict = {
-            "time": wind_forcing.time.data,
-            "lon": wind_forcing.lon.data,
-            "lat": wind_forcing.lat.data,
-        }
-        meta_dict = wind_forcing.attrs
-
-        return coord_dict, data_dict, meta_dict
+    file_structure = FileStructure(stride=6, hours_per_file=121)
