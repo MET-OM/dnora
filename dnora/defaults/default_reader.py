219a51b927463aeeedbedbc18fe5aa696f582949
Jan-Victor Björkqvist <janvb@met.no>
2025-05-19 13:53:19 +0200

    verbosity when reading environmental variables

--- dnora/defaults/default_reader.py
+++ dnora/defaults/default_reader.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import os
 import sys
 from typing import TYPE_CHECKING
-
+from dnora import msg
 if TYPE_CHECKING:
     from type_manager.dnora_types import DnoraDataType, DataSource
 
@@ -22,12 +22,22 @@ def read_defaults(filename: str, from_module: bool = False):
 
 
 def read_environment_variable(obj_type: DnoraDataType, data_source: DataSource) -> str:
+    """Reads an environmental variable:
+    1) Loads possible .env-file
+    2) Reads e.g. DNORA_LOCAL_GRID_PATH
+    3) If 2) fails, reads e.g. DNORA_LOCAL_PATH"""
     load_dotenv(f"{sys.path[0]}/.env")
-    value = os.getenv(f"DNORA_{data_source.name}_{obj_type.name}_PATH")
+    long_var = f"DNORA_{data_source.name}_{obj_type.name}_PATH"
+    short_var = f"DNORA_{data_source.name}_PATH"
+    
+    used_var = long_var
+    value = os.getenv(long_var)
+
     if value is None:
-        value = os.getenv(f"DNORA_{data_source.name}_PATH")
+        used_var = short_var
+        value = os.getenv(short_var)
     
     if value is not None:
         value = os.path.expanduser(value)
-        
+        msg.plain(f"Reading {used_var}={value}")        
     return value
