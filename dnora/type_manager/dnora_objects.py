00a130d456cf556ab616266a790aa04665e5f8bb
Jan-Victor Björkqvist <janvb@met.no>
2025-06-19 11:27:59 +0200

    added wavegrid

--- dnora/type_manager/dnora_objects.py
+++ dnora/type_manager/dnora_objects.py
@@ -9,9 +9,19 @@ from dnora.waveseries import WaveSeries
 from dnora.waterlevel import WaterLevel
 from dnora.current import Current
 from dnora.ice import Ice
+from dnora.wavegrid import WaveGrid
 
 DnoraObject = Union[
-    Grid, TriGrid, Wind, Spectra, Spectra1D, WaveSeries, WaterLevel, Current, Ice
+    Grid,
+    TriGrid,
+    Wind,
+    Spectra,
+    Spectra1D,
+    WaveSeries,
+    WaterLevel,
+    Current,
+    Ice,
+    WaveGrid,
 ]
 
 dnora_objects = {
@@ -21,6 +31,7 @@ dnora_objects = {
     DnoraDataType.SPECTRA: Spectra,
     DnoraDataType.SPECTRA1D: Spectra1D,
     DnoraDataType.WAVESERIES: WaveSeries,
+    DnoraDataType.WAVEGRID: WaveGrid,
     DnoraDataType.WATERLEVEL: WaterLevel,
     DnoraDataType.CURRENT: Current,
     DnoraDataType.ICE: Ice,
