8f8165ececd3c81a59257e4e61943048589ab262
Jan-Victor Björkqvist <janvb@met.no>
2025-06-19 10:47:14 +0200

    removed unused import

--- dnora/waveseries/waveseries.py
+++ dnora/waveseries/waveseries.py
@@ -2,9 +2,6 @@ from geo_skeletons import PointSkeleton
 from geo_skeletons.decorators import add_mask, add_time, add_datavar, add_magnitude
 import geo_parameters as gp
 
-from copy import deepcopy
-import numpy as np
-
 
 @add_magnitude(
     gp.ocean.Current, x="x_current", y="y_current", direction=gp.ocean.CurrentDir
@@ -15,6 +12,20 @@ import numpy as np
 @add_datavar(gp.wind.YWind, default_value=0.1)
 @add_datavar(gp.wind.XWind, default_value=0.1)
 @add_datavar(gp.ocean.WaterDepth)
+@add_datavar(gp.wave.Tm02Sea)
+@add_datavar(gp.wave.Tm_10Sea)
+@add_datavar(gp.wave.Tm01Sea)
+@add_datavar(gp.wave.DirmSea)
+@add_datavar(gp.wave.DirpSea)
+@add_datavar(gp.wave.TpSea)
+@add_datavar(gp.wave.HsSea)
+@add_datavar(gp.wave.Tm02Swell)
+@add_datavar(gp.wave.Tm_10Swell)
+@add_datavar(gp.wave.Tm01Swell)
+@add_datavar(gp.wave.DirmSwell)
+@add_datavar(gp.wave.DirpSwell)
+@add_datavar(gp.wave.TpSwell)
+@add_datavar(gp.wave.HsSwell)
 @add_datavar(gp.wave.Tm02)
 @add_datavar(gp.wave.Tm_10)
 @add_datavar(gp.wave.Tm01)
