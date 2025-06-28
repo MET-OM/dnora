386f514cc96d6b925edfcf5b954bf4e2e4211845
KonstantinChri <kchrist87@gmail.com>
2025-06-28 17:23:53 +0200

    add ice reader for DestinE ClimateDT

--- dnora/read/ice/destine.py
+++ dnora/read/ice/destine.py
@@ -22,15 +22,15 @@ def ds_polytope_read(
 ):
 
     grib_file = download_ecmwf_from_destine(
-        start_time, end_time, lon=lon, lat=lat, folder="dnora_wind_temp"
+        start_time, end_time, lon=lon, lat=lat, folder="dnora_ice_temp"
     )
 
     ds = xr.open_dataset(grib_file, engine="cfgrib", decode_timedelta=True)
-    lons, lats, u10, v10 = (
-        ds.u10.longitude.values,
-        ds.u10.latitude.values,
-        ds.u10.values,
-        ds.v10.values,
+    lons, lats, sic = (
+        ds.siconc.longitude.values,
+        ds.siconc.latitude.values,
+        ds.siconc.values,
+        #ds.sit.values,
     )
     native_dlon, native_dlat = 1 / 8, 1 / 30
     xi = np.arange(min(lons), max(lons), native_dlon)
@@ -38,20 +38,20 @@ def ds_polytope_read(
     Xi, Yi = np.meshgrid(xi, yi)
 
     Nt = len(ds.step)
-    u10i = np.zeros((Nt, len(yi), len(xi)))
-    v10i = np.zeros((Nt, len(yi), len(xi)))
+    sici = np.zeros((Nt, len(yi), len(xi)))
+    #siti = np.zeros((Nt, len(yi), len(xi)))
     # If this becomes slow, we need to think about 3D interpolation / resuing weights
     for n in range(Nt):
-        u10i[n, :, :] = griddata(
-            list(zip(lons, lats)), u10[n, :], (Xi, Yi), method="nearest"
+        sici[n, :, :] = griddata(
+            list(zip(lons, lats)), sic[n, :], (Xi, Yi), method="nearest"
         )
-        v10i[n, :, :] = griddata(
-            list(zip(lons, lats)), v10[n, :], (Xi, Yi), method="nearest"
-        )
+        #siti[n, :, :] = griddata(
+        #    list(zip(lons, lats)), sit[n, :], (Xi, Yi), method="nearest"
+        #)
 
     data = dnora_class(lon=xi, lat=yi, time=ds.time + ds.step)
-    data.set_u(u10i)
-    data.set_v(v10i)
+    data.set_sic(sici)
+    #data.set_sit(siti)
     lo, la = utils.grid.expand_area(
         lon, lat, expansion_factor=1, dlon=native_dlon, dlat=native_dlat
     )
@@ -61,7 +61,7 @@ def ds_polytope_read(
 
 
 def download_ecmwf_from_destine(start_time, end_time, lon, lat, folder: str) -> str:
-    """Downloads 10 m wind data DestinE ClimateDT data portfolio data from the Destine Earth Store System  for a
+    """Downloads DestinE ClimateDT data portfolio data from the Destine Earth Store System for a
     given area and time period"""
     start_time = pd.Timestamp(start_time)
     end_time = pd.Timestamp(end_time)
@@ -74,16 +74,16 @@ def download_ecmwf_from_destine(start_time, end_time, lon, lat, folder: str) -> 
         raise e
     c = Client(address="polytope.lumi.apps.dte.destination-earth.eu")
 
-    filename = f"{folder}/ECMWF_temp.grib"  # Switch to this in production. Then the files will be cleaned out
+    filename = f"{folder}/ECMWF_temp_ice.grib"  # Switch to this in production. Then the files will be cleaned out
     # filename = f"{folder}/destine_temp.grib"
-    request_winds = {
+    request_ice = {
         "class": "d1",
         "expver": "0001",
         "dataset": "extremes-dt",
         "stream": "oper",
         "type": "fc",
         "levtype": "sfc",
-        "param": "165/166",
+        "param": "31",
         "time": "00",
         "step": "0/1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23",
         "area": [
@@ -94,9 +94,9 @@ def download_ecmwf_from_destine(start_time, end_time, lon, lat, folder: str) -> 
         ],
     }
     date_str = start_time.strftime("%Y%m%d")
-    request_winds["date"] = date_str
+    request_ice["date"] = date_str
 
-    c.retrieve("destination-earth", request_winds, filename)
+    c.retrieve("destination-earth", request_ice, filename)
     return filename
 
 
