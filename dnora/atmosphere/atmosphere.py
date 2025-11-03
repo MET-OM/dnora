23a4c1940ece857eab885b11415837103af3d8dd
Jan-Victor Björkqvist <janvb@met.no>
2025-11-03 12:36:58 +0100

    added atmosphere class

---
+++ dnora/atmosphere/atmosphere.py
@@ -0,0 +1,11 @@
+from geo_skeletons import GriddedSkeleton
+from geo_skeletons.decorators import add_time, add_datavar, add_magnitude
+import geo_parameters as gp
+import numpy as np
+
+
+@add_datavar(name=gp.atm.RelativeHumidity("r"), default_value=np.nan)
+@add_datavar(name=gp.atm.AirTemperature("t2m"), default_value=np.nan)
+@add_time(grid_coord=True)
+class Atmosphere(GriddedSkeleton):
+    pass
