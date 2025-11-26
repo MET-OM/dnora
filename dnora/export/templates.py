ad9043e5ae610861f8f1e97449e390c85ec85713
Jan-Victor Björkqvist <janvb@met.no>
2025-11-26 13:40:37 +0100

    fixed filename errors in writing monthly netcdf files

--- dnora/export/templates.py
+++ dnora/export/templates.py
@@ -20,7 +20,7 @@ class Cacher(DataExporter):
     _silent = True
 
     def _get_default_writer(self) -> WriterFunction:
-        return data_writers.Netcdf(daily_files=True)
+        return data_writers.Netcdf(monthly_files=True)
 
     def _get_default_format(self) -> str:
         return ModelFormat.CACHE
