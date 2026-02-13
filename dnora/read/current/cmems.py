5973ef200e8eb3997aadb093dfd55f2a91c3c5ec
Jan-Victor Björkqvist <janvb@met.no>
2026-02-13 14:41:49 +0100

    updated doc strings

--- dnora/read/current/cmems.py
+++ dnora/read/current/cmems.py
@@ -39,15 +39,17 @@ class Global(ProductReader):
 
 @deprecated_class_call("CMEMS", "cmems", "current")
 class EuropeNW(ProductReader):
-    """This product is a L4 REP and NRT global total velocity field at 0m and 15m together wiht its individual components 
-    (geostrophy and Ekman) and related uncertainties. It consists of the zonal and meridional velocity at a 1h frequency 
-    and at 1/4 degree regular grid. The total velocity fields are obtained by combining CMEMS satellite Geostrophic surface 
-    currents and modelled Ekman currents at the surface and 15m depth (using ERA5 wind stress in REP and ERA5* in NRT). 
-    1 hourly product, daily and monthly means are available. This product has been initiated in the frame of CNES/CLS projects. 
-    Then it has been consolidated during the Globcurrent project (funded by the ESA User Element Program)..
+    """The ocean physics reanalysis for the North-West European Shelf is produced using an ocean assimilation model, with tides, at 7 km horizontal resolution.
+    The ocean model is NEMO (Nucleus for European Modelling of the Ocean), using the 3DVar NEMOVAR system to assimilate observations. 
+    These are surface temperature and vertical profiles of temperature and salinity. The model is forced by lateral boundary conditions from the 
+    GloSea5, one of the multi-models used by GLOBAL_REANALYSIS_PHY_001_026 and at the Baltic boundary by the BALTICSEA_REANALYSIS_PHY_003_011. 
+    The atmospheric forcing is given by the ECMWF ERA5 atmospheric reanalysis. The river discharge is from a daily climatology.
 
-    DOI (product): https://doi.org/10.48670/mds-00327
-    https://https://data.marine.copernicus.eu/product/MULTIOBS_GLO_PHY_MYNRT_015_003/description
+    
+    Note: This reader reads the hourly 2D surface fields of surface currents.
+
+    DOI (product): https://doi.org/10.48670/moi-00059
+    https://data.marine.copernicus.eu/product/NWSHELF_MULTIYEAR_PHY_004_009/description
     """
 
     product_configuration = ProductConfiguration(
--- dnora/read/waterlevel/cmems.py
+++ dnora/read/waterlevel/cmems.py
@@ -42,17 +42,17 @@ class Global(ProductReader):
 
 @deprecated_class_call("CMEMS", "cmems", "waterlevel")
 class EuropeNW(ProductReader):
-    """The Operational Mercator global ocean analysis and forecast system at 1/12 degree is providing 10 days of 3D global
-    ocean forecasts updated daily. The time series is aggregated in time in order to reach a two full year's time series
-    sliding window. This product includes daily and monthly mean files of temperature, salinity, currents, sea level,
-    mixed layer depth and ice parameters from the top to the bottom over the global ocean. It also includes hourly
-    mean surface fields for sea level height, temperature and currents. The global ocean output files are displayed
-    with a 1/12 degree horizontal resolution with regular longitude/latitude equirectangular projection.
-    50 vertical levels are ranging from 0 to 5500 meters. This product also delivers a special dataset for
-    surface current which also includes wave and tidal drift called SMOC (Surface merged Ocean Current).
+    """The ocean physics reanalysis for the North-West European Shelf is produced using an ocean assimilation model, with tides, at 7 km horizontal resolution.
+    The ocean model is NEMO (Nucleus for European Modelling of the Ocean), using the 3DVar NEMOVAR system to assimilate observations. 
+    These are surface temperature and vertical profiles of temperature and salinity. The model is forced by lateral boundary conditions from the 
+    GloSea5, one of the multi-models used by GLOBAL_REANALYSIS_PHY_001_026 and at the Baltic boundary by the BALTICSEA_REANALYSIS_PHY_003_011. 
+    The atmospheric forcing is given by the ECMWF ERA5 atmospheric reanalysis. The river discharge is from a daily climatology.
 
-    DOI (product): https://doi.org/10.48670/moi-00016
-    https://data.marine.copernicus.eu/product/GLOBAL_ANALYSISFORECAST_PHY_001_024/description
+    
+    Note: This reader reads the hourly 2D surface fields of sea surface height.
+
+    DOI (product): https://doi.org/10.48670/moi-00059
+    https://data.marine.copernicus.eu/product/NWSHELF_MULTIYEAR_PHY_004_009/description
     """
 
     product_configuration = ProductConfiguration(
